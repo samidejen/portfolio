a7f43a20ed4652cba83e95bb073ef65886d25129
sami <sami@kali.sami>
2025-07-01 15:31:51 -0400

    first commit

---
+++ components/about.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+<p className="mb-3 leading-relaxed text-lg text-gray-700 dark:text-gray-300">
+  I’m a passionate, self-taught <span className="font-semibold text-primary">Penetration Tester</span> with a solid foundation in 
+  <span className="font-semibold text-primary"> Vulnerability Assessment and Penetration Testing (VAPT)</span>. Currently pursuing a 
+  <span className="font-semibold text-primary"> BSc in Computer Science</span> in Ethiopia, I continuously enhance my practical skills 
+  by solving real-world <span className="font-semibold text-primary">Capture The Flag (CTF)</span> challenges on platforms like 
+  <span className="font-semibold text-primary"> Hack The Box</span> and <span className="font-semibold text-primary">TryHackMe</span>. 
+  I’m deeply interested in ethical hacking, network security, and hands-on exploitation techniques.
+</p>
+
+
+      
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,136 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import cipherGuard from "@/public/cipherGuard.png";
+import portfolio from "@/public/portfolio.png";
+import StudyNotion from "@/public/StudyNotion.png"
+import MahiStoreAdmin from "@/public/Admin.png"
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+
+
+export const experiencesData = [
+  {
+    title: "Full-Stack Developer Intern",
+    location: "Debark University, Gonder, Ethiopia (On-site)",
+    description: `
+      Developed a Student Management Portal using PHP, MySQL, and JavaScript. 
+      Implemented secure authentication and role-based access control. 
+      Optimized database performance to improve query speed and overall efficiency.
+    `,
+    icon: React.createElement(CgWorkAlt),
+    date: "June 2023 - September 2023",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Remote",
+    description: `
+      Designed and developed a responsive eCommerce platform using React, Tailwind CSS, and TypeScript. 
+      Built secure payment integrations using Stripe and Chapa, enhanced SEO, and improved application performance. 
+      Worked on a complete platform rewrite including Node.js, Express, and MongoDB.
+    `,
+    icon: React.createElement(CgWorkAlt),
+    date: "May 2024 - September 2024",
+  },
+  {
+    title: "Network Engineering Intern",
+    location: "onsite",
+    description: `
+   ssisted in designing, configuring, and securing the university's internal network infrastructure. 
+    Implemented VLANs and firewall rules to segment traffic and enhance security. 
+    Conducted routine diagnostics, documented topology changes, and supported troubleshooting 
+    for routers, switches, and wireless access points across multiple departments.
+    `,
+    icon: React.createElement(CgWorkAlt),
+    date: "August 2023 - December 2023",
+  },
+  {
+    title: "Associate Cyber Security Consultant",
+    location: "Remote",
+    description: `
+      I worked as an Associate Cyber Security Consultant at Security Brigade Infosec.
+    `,
+    icon: React.createElement(CgWorkAlt),
+    date: "October 2024 - Present",
+  },{
+  title: "Nanodegree Graduate – Programming Foundations",
+  location: "Udacity (Online)",
+  description: `
+    Completed the Udacity Fundamentals of Programming Nanodegree Program, gaining a strong 
+    foundation in Python, problem-solving, control flow, data structures, and basic algorithms. 
+    Built interactive mini-projects and scripts that demonstrated logical thinking and clean code practices.
+  `,
+  icon: React.createElement(CgWorkAlt) ,
+  date: "August 2024",
+}
+
+
+] as const;
+
+
+export const projectsData = [
+  {
+  title: "Mahi-STORE ADMIN",
+  description: "the admin-facing dashboard of the platform, built to manage products, users, and orders. It empowers administrators with tools for content control, analytics, and a seamless user experience. Key features include product management with image uploads, role-based access control, order tracking, and real-time analytics dashboards powered by charts and data caching.",
+  tags: ["NodeJS", "ExpressJS", "MongoDB", "TailwindCSS", "Redux Toolkit", "TypeScript", "Next.js", "Mongoose"],
+
+
+  imageUrl: MahiStoreAdmin, // Replace with actual image import or path
+  liveUrl: "https://mahieco.onrender.com/", // Replace with actual deployed link
+}
+,
+  {
+    title: "CipherGuard",
+    description:
+      "Cipher Guard is a cutting-edge security application designed for robust data protection. Built on Mern , this project offers a modern and efficient platform to secure digital credentials.",
+    tags: ["React", "MongoDB", "Tailwind", "Express"],
+    imageUrl: cipherGuard,
+    liveUrl:"https://cipher-guard.netlify.app",
+  },
+  {
+    title: "Personal Portfolio",
+    description:
+      "I Build this Personal Portfolio to showcase my skills and interests to the world.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind",],
+    imageUrl: portfolio,
+    liveUrl:"https://samidejen.vercel.app",
+  },
+  {
+ 
+  title: "Mahi-STORE USER",
+  description: "MahiStoreUser website is a digital platform that facilitates online buying and selling of goods and services. These websites enable businesses to reach a global audience and provide consumers with a convenient and efficient shopping experience from the comfort of their homes.",
+  tags: ["ReactJS", "NodeJS", "MongoDB", "ExpressJS"],
+  imageUrl: StudyNotion,  // keep the same image or change if you have a new one
+  liveUrl: "https://mahistore1.onrender.com/",
+}
+
+] as const;
+
+export const skillsData = [
+  "Javascript", "Python", "Bash", "HTML", "CSS", "Express.js", "Next.js", "React.js", "NoSQL", "SQL", "Linux", "Windows", "Penetration Testing",  "Exploitation", "Privilege Escalation", "Vulnerability Scanning", "Tunneling", "Kali Linux", "Nmap", "OWASP Top 10", "Wireshark", "Burpsuite", "Metasploit", "Nikto", "Hashcat", "FFUF","Aircrack-ng", "SQLMap", "Git", "GitHub", "Postman", "Figma", "VS Code", 
+] as const;
